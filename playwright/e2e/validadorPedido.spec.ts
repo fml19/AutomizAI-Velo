d0d262fc7785b3870f26bdd63c4a67be2457df7c
Seu Nome <vet.fml@gmail.com>
2026-06-26 10:03:56 -0300

    AAA - Arrange(Preparar), Act(Agir), Assert(Verificar)

--- playwright/e2e/validadorPedido.spec.ts
+++ playwright/e2e/validadorPedido.spec.ts
@@ -1,14 +1,20 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange(Preparar), Act(Agir), Assert(Verificar)
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+
+  // Arrange
   await page.goto('http://localhost:5173/');
-  //Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+
+  // Act
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-   //Checkpoint
   await expect(page.getByRole('heading', { name: 'Consultar Pedido' })).toBeVisible();
   await page.getByTestId('search-order-id').fill('VLO-Z69QA0');
   await page.getByTestId('search-order-button').click();
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-Z69QA0');
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 });
